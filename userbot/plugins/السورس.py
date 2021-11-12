2aa7bcedfab961a1d458a5c92dded066a089f7ff
dse1 <94114766+dse1@users.noreply.github.com>
2021-11-12 05:34:46 +0300

    Update السورس.py

--- userbot/plugins/السورس.py
+++ userbot/plugins/السورس.py
@@ -4,7 +4,7 @@ from Jmthon.razan.resources.mybot import *
 
 from ..Config import Config
 
-ROZ_PIC = "https://telegra.ph/file/5f6ef13851dcf0d6fc72b.jpg"
+ROZ_PIC = "https://telegra.ph/file/0dce2b7aaadc56eac9173.jpg"
 
 if Config.TG_BOT_USERNAME is not None and tgbot is not None:
 
@@ -17,8 +17,8 @@ if Config.TG_BOT_USERNAME is not None and tgbot is not None:
         if query.startswith("السورس") and event.query.user_id == bot.uid:
             buttons = [
                 [
-                    Button.url("قنـاة السـورس ⚙️", "https://t.me/JMTHON"),
-                    Button.url("المطـور 👨🏼‍💻", "https://t.me/RR7PP"),
+                    Button.url("𝗙𝗲𝗲𝗹𝗶𝗻𝗴", "https://t.me/NvvvM"),
+                    Button.url("𝗠𝘂𝘀𝘁𝗲𝗿", "https://t.me/C1CiC"),
                 ]
             ]
             if ROZ_PIC and ROZ_PIC.endswith((".jpg", ".png", "gif", "mp4")):
